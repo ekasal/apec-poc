da6cfa2d7fc0453c955b0480f225115af6f5aeef
gregory <1063530+gqio@users.noreply.github.com>
2023-04-12 18:05:52 +0200

    wip8

--- src/components/si/multiple-checkbox/multiple-checkbox.stories.ts
+++ src/components/si/multiple-checkbox/multiple-checkbox.stories.ts
@@ -4,20 +4,23 @@ import { StoryFn, Meta } from "@storybook/angular";
 export default {
   title: "SI/MultipleCheckboxComponent",
   component: MultipleCheckboxComponent,
-  argTypes: {},
+  argTypes: {
+    label: { type: "string", defaultValue: "Label" },
+    selected: { type: "number" },
+  },
 } as Meta;
 
-export const Playground: StoryFn<MultipleCheckboxComponent> = (
-  args: MultipleCheckboxComponent
-) => ({
-  props: {
-    ...args,
-    label: "Mutiple Checkbox",
-    idsNom: ["1"],
-    data: [
-      { idNomenclature: "1", libelle: "Label 1" },
-      { idNomenclature: "2", libelle: "Label 2" },
-      { idNomenclature: "3", libelle: "Label 3" },
-    ],
-  },
-});
+export const Playground: StoryFn<MultipleCheckboxComponent> = (args: any) => {
+  const data = [
+    { idNomenclature: "1", libelle: "Label 1" },
+    { idNomenclature: "2", libelle: "Label 2" },
+    { idNomenclature: "3", libelle: "Label 3" },
+  ];
+  return {
+    props: {
+      ...args,
+      idsNom: args.selected ? data[args.selected].idNomenclature : undefined,
+      data,
+    },
+  };
+};
--- src/components/si/slidebox/slidebox.stories.ts
+++ src/components/si/slidebox/slidebox.stories.ts
@@ -4,9 +4,14 @@ import { SlideboxComponent } from "./slidebox.component";
 export default {
   title: "SI/SlideboxComponent",
   component: SlideboxComponent,
-  argTypes: {},
+  argTypes: {
+    label: { type: "string", defaultValue: "Slidebox" },
+    disabled: { type: "boolean", defaultValue: false },
+    dataOn: { type: "string", defaultValue: "ON" },
+    dataOff: { type: "string", defaultValue: "OFF" },
+  },
 } as Meta;
 
 export const Playground: StoryFn<SlideboxComponent> = (
   args: SlideboxComponent
-) => ({ props: { ...args, label: "Slidebox" } });
+) => ({ props: { ...args } });
